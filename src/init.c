b2dc7d4be57a9890c719f587859a7dd1956f3e3f
Huntears <dev@huntears.com>
2020-10-09 13:30:46 +0200

    Add documentation for the initialization

--- src/init.c
+++ src/init.c
@@ -7,6 +7,12 @@
 
 #include "my_logger.h"
 
+/// \brief Inits the logger
+/// \param filename The filename to use (Can be set to NULL if no print to file)
+/// \param append Appends or not to an existing file
+/// \param print_to_stdout Prints on stdout
+/// \param print_to_file Prints into file
+/// \return The logger
 logger_t *init_logger(char *filename, bool append, bool print_to_stdout, bool print_to_file)
 {
     logger_t *logger = malloc(sizeof(logger_t));
